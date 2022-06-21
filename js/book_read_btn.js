7fc3a1db4703bd78126a5913cf6775ccc3c14557
MIMIKorig <39917720+MIMIKorig@users.noreply.github.com>
2022-06-22 04:37:51 +0600

    Add files via upload

---
+++ js/book_read_btn.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($){
+  
+    $('.book_btn_read').on('click', function(e){
+      alert('Чтение книги на данный момент не возможен, повторите попытку чуть позжее');
+    })
+    
+  });
